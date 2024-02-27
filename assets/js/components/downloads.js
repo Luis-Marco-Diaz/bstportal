ddba0f73955b3858c702dd340ecd7ddd1bfb5841
Marco <marko.dzm@gmail.com>
2024-02-27 14:30:57 -0600

    blog

---
+++ assets/js/components/downloads.js
@@ -0,0 +1,38 @@
+//ARTICLES DOWNLOADS
+
+// DROPBOX MAGAZINE - ENGLISH
+function MagazineEnglish(url) {
+    var url = "https://www.dropbox.com/scl/fi/emkgt3mtm89fls8gxaqpg/THE-BST-MAGAZINE.pdf?rlkey=5b5aorbzkq2pixwc4am5951l5&dl=0";
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+// DROPBOX MAGAZINE - SPANISH
+  function MagazineSpanish(url) {
+    var url = "https://www.dropbox.com/scl/fi/qs1yxankpudh1thp0tmja/LA-REVISTA-BST.pdf?rlkey=be6uh0i80wuuvbpiqr6u2qw9a&dl=0";
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+// DROPBOX MAGAZINE - FRENCH
+  function MagazineFrench(url) {
+    var url = "https://www.dropbox.com/scl/fi/x7vub34otuyvksjwbdb4a/LE-MAGAZINE-BST.pdf?rlkey=8hmn9gr3zfcgqdahjyews0wai&dl=0";
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+// DROPBOX MAGAZINE - GERMAN
+function MagazineGerman(url) {
+    var url = "https://www.dropbox.com/scl/fi/zud288e8uefzwt53439nz/DAS-BST-MAGAZIN.pdf?rlkey=5m195shpxey96qcum3cobvigq&dl=0";
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+// DROPBOX MAGAZINE - ITALIAN
+  function MagazineItalian(url) {
+    var url = "https://www.dropbox.com/scl/fi/us7ftdf39rphr8qdw6urr/LA-RIVISTA-BST.pdf?rlkey=ajamm355mqq6t20szedzawvto&dl=0";
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+// DROPBOX MAGAZINE - PORTUGUESE
+  function MagazinePortuguese(url) {
+    var url = "https://www.dropbox.com/scl/fi/0gw7cmydma2sxjna3cmit/A-REVISTA-BST.pdf?rlkey=usqorcexs36o89pxaj2shmlky&dl=0";
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
